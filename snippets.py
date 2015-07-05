a1da00edf7c20b0fb4a2de9d51dd789981f17d54
aeng <ae7379@gmail.com>
2015-07-05 18:38:03 +0000

    parser

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -14,10 +16,26 @@ def put(name, snippet):
     
 def get(name):
     """Retrieve the snippet with a given name.
-
     If there is no such snippet return error message.
-
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
